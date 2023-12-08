7aecdbb562b460cda672dbc9050fe7be27df8b29
gpzeke <65458253+gpzeke@users.noreply.github.com>
2023-12-08 10:26:59 -0600

    Compliment initialization

---
+++ user.py
@@ -0,0 +1,9 @@
+# Define the user profile
+
+class User:
+    def __init__(self, first_name, last_name, nickname, date_of_birth, preferred_name=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.date_of_birth = date_of_birth
+        self.preferred_name = preferred_name or first_name
---
+++ user_data.py
@@ -0,0 +1,25 @@
+# Read and write user save information
+import json
+
+class UserData:     # handles the persistance of user data
+    def __init__(self, filename="users.json"):
+        self.filename = filename
+
+    def save_users(self, users):    # saves a list of user objects
+        with open(self.filename, "w") as file:  # with ensure closes properly after execution
+            user_data = [{   "first_name": user.first_name,
+                            "last_name":user.last_name,
+                            "nickname":user.nickname,
+                            "date_of_birth":user.date_of_birth,
+                            "preferred_name":user.preferred_name}
+                        for user in users]  # creates a list [] of dictionaries {}
+            json.dump(user_data, file)  # serializes user_data and writes to file
+
+    def load_users(self):   # loads user data from file
+        try:
+            with open(self.filename, "r") as file:
+                user_data = json.load(file)
+                users = [User(**data) for data in user_data]    # **data syntax is used to unpack the dictionary values as keyword arguments when creating the user object
+                return users
+        except FileNotFoundError:
+            return []
