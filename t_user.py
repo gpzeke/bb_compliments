26d04a857b36a6484ec0d02081ec5cc0e70082aa
gpzeke <65458253+gpzeke@users.noreply.github.com>
2023-12-09 15:07:44 -0600

    fixed imports

--- t_user.py
+++ t_user.py
@@ -1,5 +1,5 @@
-from BbCompliments.user_creation import create_user
-from BbCompliments.user_data_management import UserData
+from user_creation import create_user
+from user_data_management import UserData
 
 def main():
     # Create a user
