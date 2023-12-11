ff33c5e97b807655d0fcc502264ad458eda967f7
gpzeke <65458253+gpzeke@users.noreply.github.com>
2023-12-10 20:37:59 -0600

    added date validation to user creation

--- user_creation.py
+++ user_creation.py
@@ -1,11 +1,20 @@
 # Create a user
 from user import User
+from datetime import datetime
 
 def create_user():
     first_name = input("What is your first name?\n")
     last_name = input("What is your last name?\n")
     nickname = input("What is your nickname?\n")
-    date_of_birth = input("When were you born (YYYY-MM-DD)?\n")
+
+    while True:
+        date_of_birth = input("When were you born (YYYY-MM-DD)?\n")
+
+        try:
+            datetime.strptime(date_of_birth, '%Y-%m-%d')
+            break
+        except ValueError:
+            print("Invalid date format, please enter the date in YYYY-MM-DD format.")
 
     while True:
         print("Would you like me to refer to you by your first name or your nickname?")
@@ -18,5 +27,12 @@ def create_user():
             break
         else:
             print("Invalid selection, please enter 1 or 2.")
-
     preferred_name = first_name if preference_choice == '1' else nickname
+    
+    return User(
+    first_name=first_name,
+    last_name=last_name,
+    nickname=nickname,
+    date_of_birth=date_of_birth,
+    preferred_name=preferred_name
+)
